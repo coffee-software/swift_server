27d6f99e73d9f6b4959401b7d30c7502d353a0ad
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2023-03-13 12:07:50 +0100

    debug, print mailer

--- lib/mailer.dart
+++ lib/mailer.dart
@@ -19,6 +19,44 @@ abstract class Mailer {
         Iterable<String> replyTo = const []
       }
       ) async {
+
+    var type = config.getRequired<String>('mailer.type');
+    if (type == 'smtp') {
+      return await _sendSmtpEmail(subject, bodyHtml, recipients, replyTo: replyTo);
+    } else if (type == 'print') {
+      return await _printEmail(subject, bodyHtml, recipients, replyTo: replyTo);
+    } else {
+      throw Exception('undefined mailer type');
+    }
+  }
+
+  Future<bool> _printEmail(
+      String subject,
+      String bodyHtml,
+      Iterable<String> recipients,
+      {
+        Iterable<String> replyTo = const []
+      }
+      ) async {
+    print('################## SENDING EMAIL ##################');
+    print('# subject: ' + subject);
+    print('# recipients: ' + recipients.join(','));
+    print('# replyTo: ' + replyTo.join(','));
+    print('##################    BODY       ##################');
+    print(bodyHtml);
+    print('###################################################');
+    return true;
+  }
+
+  Future<bool> _sendSmtpEmail(
+      String subject,
+      String bodyHtml,
+      Iterable<String> recipients,
+      {
+        Iterable<String> replyTo = const []
+      }
+      ) async {
+
     var smtpServer = new SmtpServer(
         config.getRequired<String>('mailer.hostName'),
         name: config.getRequired<String>('mailer.sender.email'),
@@ -34,11 +72,11 @@ abstract class Mailer {
       )
       ..recipients.addAll(recipients)
       ..subject = subject
-      //TODO html stripper:
+    //TODO html stripper:
       ..text = bodyHtml.replaceAll(RegExp(
           r"<[^>]*>",
           multiLine: true
-        ), '')
+      ), '')
       ..html = bodyHtml;
 
     if (replyTo.isNotEmpty) {
@@ -51,4 +89,5 @@ abstract class Mailer {
     await send(emailMessage, smtpServer);
     return true;
   }
+
 }
