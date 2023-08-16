69e6d543bd02ea95f97857b9158870cd6e999a31
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2023-08-16 17:26:33 +0200

    schema format for new migration script

--- config/schema.sql
+++ config/schema.sql
@@ -4,21 +4,21 @@
 
 DROP TABLE IF EXISTS `run_errors`;
 CREATE TABLE `run_errors` (
-  `app_id` int NOT NULL,
+  `app_id` int(11) NOT NULL,
   `handler` varchar(255) NOT NULL,
   `location` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
-  `current_count` int NOT NULL DEFAULT '1',
-  `total_count` int NOT NULL DEFAULT '1',
-  `first_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `first_message` text,
-  `first_stack` text,
-  `first_request` text,
-  `last_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `last_message` text,
-  `last_stack` text,
-  `last_request` text,
-  `comment` text,
+  `current_count` int(11) NOT NULL DEFAULT 1,
+  `total_count` int(11) NOT NULL DEFAULT 1,
+  `first_time` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `first_message` text DEFAULT NULL,
+  `first_stack` text DEFAULT NULL,
+  `first_request` text DEFAULT NULL,
+  `last_time` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `last_message` text DEFAULT NULL,
+  `last_stack` text DEFAULT NULL,
+  `last_request` text DEFAULT NULL,
+  `comment` text DEFAULT NULL,
   PRIMARY KEY (`app_id`,`handler`,`location`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -28,10 +28,10 @@ CREATE TABLE `run_errors` (
 
 DROP TABLE IF EXISTS `run_jobs`;
 CREATE TABLE `run_jobs` (
-  `app_id` int NOT NULL,
+  `app_id` int(11) NOT NULL,
   `job` varchar(255) NOT NULL,
-  `run_count` int NOT NULL DEFAULT '1',
-  `last_run` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `run_count` int(11) NOT NULL DEFAULT 1,
+  `last_run` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`app_id`,`job`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -41,10 +41,10 @@ CREATE TABLE `run_jobs` (
 
 DROP TABLE IF EXISTS `run_queues`;
 CREATE TABLE `run_queues` (
-  `app_id` int NOT NULL,
+  `app_id` int(11) NOT NULL,
   `queue` varchar(255) NOT NULL,
-  `process_count` int NOT NULL DEFAULT '1',
-  `last_process` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `process_count` int(11) NOT NULL DEFAULT 1,
+  `last_process` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`app_id`,`queue`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -55,13 +55,13 @@ CREATE TABLE `run_queues` (
 DROP TABLE IF EXISTS `run_stats`;
 CREATE TABLE `run_stats` (
   `time` datetime NOT NULL,
-  `app_id` int NOT NULL,
-  `sub_id` int NOT NULL,
+  `app_id` int(11) NOT NULL,
+  `sub_id` int(11) NOT NULL,
   `handler` varchar(255) NOT NULL,
-  `count` int NOT NULL,
-  `max_queries` int NOT NULL,
+  `count` int(11) NOT NULL,
+  `max_queries` int(11) NOT NULL,
   `total_queries` float NOT NULL,
-  `max_time` int NOT NULL,
+  `max_time` int(11) NOT NULL,
   `total_time` float NOT NULL,
   PRIMARY KEY (`time`,`app_id`,`sub_id`,`handler`),
   KEY `handler` (`handler`)
