0aaf494a2bf1d338a8bc04e3e5389ccf77fcbc15
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2023-08-18 16:57:31 +0200

    cleanup cli commands, add help and usage info

--- lib/cli.dart
+++ lib/cli.dart
@@ -1,5 +1,7 @@
 library c7server;
 
+import 'dart:io';
+
 import 'package:args/args.dart';
 export 'package:args/args.dart';
 
@@ -7,13 +9,20 @@ import 'package:swift_composer/swift_composer.dart';
 export 'package:swift_composer/swift_composer.dart';
 import 'package:swift_server/config.dart';
 export 'package:swift_server/config.dart';
+import 'package:path/path.dart' as path;
 
 import 'tools.dart';
 export 'tools.dart';
 export 'mailer.dart';
 
 const CliArg = true;
+const CliParameter = true;
 const CliParameters = true;
+
+class HelpText {
+  final String value;
+  const HelpText(this.value);
+}
 
 /**
  * Single Command
@@ -27,6 +36,14 @@ abstract class Command {
   Cli get cli;
 
   Future run();
+
+  int paramI = 1;
+
+  List<String> get params {
+    List<String> params = [];
+    configureCliParams(params);
+    return params;
+  }
 
   @Compile
   void setCliArgs(ArgResults args);
@@ -59,28 +76,51 @@ abstract class Command {
     field = args.rest.sublist(1);
   }
 
+  @CompileFieldsOfType
+  @AnnotatedWith(CliParameter)
+  // ignore: unused_element
+  void _setCliArgsParameterString(ArgResults args, String name, String field) {
+    field = args.rest[paramI++];
+  }
+
   @Compile
   void configureCliArgs(ArgParser parser);
 
+  @Compile
+  void configureCliParams(List<String> params);
+
   @CompileFieldsOfType
   @AnnotatedWith(CliArg)
   // ignore: unused_element
-  void _configureCliArgsStringRequired(ArgParser parser, String name, String field) {
-    parser.addOption(name, mandatory:true);
+  void _configureCliArgsStringRequired(ArgParser parser, String name, String field, {String HelpText_value = ''}) {
+    parser.addOption(name, help: HelpText_value, valueHelp:name, mandatory:true);
   }
 
   @CompileFieldsOfType
   @AnnotatedWith(CliArg)
   // ignore: unused_element
-  void _configureCliArgsStringOptional(ArgParser parser, String name, String? field) {
-    parser.addOption(name, mandatory:false);
+  void _configureCliArgsStringOptional(ArgParser parser, String name, String? field, {String HelpText_value = ''}) {
+    parser.addOption(name, help: HelpText_value, valueHelp:name, mandatory:false);
   }
 
   @CompileFieldsOfType
   @AnnotatedWith(CliArg)
   // ignore: unused_element
-  void _configureCliArgsBool(ArgParser parser, String name, bool field) {
-    parser.addFlag(name);
+  void _configureCliArgsBool(ArgParser parser, String name, bool field, {String HelpText_value = ''}) {
+    parser.addFlag(name, help: HelpText_value);
+  }
+
+  @CompileFieldsOfType
+  @AnnotatedWith(CliParameter)
+  // ignore: unused_element
+  void _configureCliParamsString(List<String> params, String name, String field, {String HelpText_value = ''}) {
+    params.add(name);
+  }
+  @CompileFieldsOfType
+  @AnnotatedWith(CliParameters)
+  // ignore: unused_element
+  void _configureCliParamsStringsList(List<String> params, String name, List<String> field, {String HelpText_value = ''}) {
+    params.add(name + ',..');
   }
 }
 
@@ -101,22 +141,79 @@ abstract class Cli {
   @InjectInstances
   Map<String, Command> get allCommands;
 
-  Future run(List<String> arguments) async {
-    if (arguments.length < 1 || !allCommands.containsKey(arguments[0])) {
-      allCommands.forEach((key, value) {
-        print(key);
-      });
-      throw new Exception('unknown command');
-    }
-    var command = allCommands[arguments[0]]!;
+  @Inject
+  SubtypesOf<Command> get availableCommands;
 
+  String commandToClassCode(String command) {
+    var bits = command.split(':');
+    if (bits.length > 1) {
+      bits[0] = 'module_' + bits[0];
+      bits[1] = bits[1][0].toUpperCase() + bits[1].substring(1);
+    } else {
+      bits[0] = bits[0][0].toUpperCase() + bits[0].substring(1);
+    }
+    return bits.join('.');
+  }
+
+  String classCodeToCommand(String code) {
+    var bits = code.split('.');
+    bits[0] = bits[0].replaceFirst('module_', '');
+    return bits.map((e) => e[0].toLowerCase() + e.substring(1)).join(':');
+  }
+
+  ArgParser getCommandArgParser(Command command) {
     var parser = ArgParser();
-    parser.addOption('config', mandatory:true);
+    parser.addOption('config', valueHelp: 'path', help: 'path to config file, defaults to \'config.yaml\'', mandatory:false);
     command.configureCliArgs(parser);
+    return parser;
+  }
+
+  Future run(List<String> arguments) async {
+    if (arguments.length < 1) {
+      print('available commands:');
+      availableCommands.allSubtypes.forEach((key, info) {
+        String help = info.annotations.containsKey('HelpText') ? info.annotations['HelpText'] : '';
+        print(classCodeToCommand(key) + ' ' + allCommands[key]!.params.map((e) => '[$e]').join(' ') + '\t' + help);
+      });
+      return;
+    }
+    String classCode = commandToClassCode(arguments[0]);
+    if (!allCommands.containsKey(classCode)) {
+      throw new Exception('unknown command ${arguments[0]}');
+    }
+    var command = allCommands[classCode]!;
+
+    var parser = getCommandArgParser(command);
+
     ArgResults args = parser.parse(arguments);
-    await config.load(args['config']);
+    await config.load(args['config'] ?? path.dirname(Platform.script.toFilePath()) + '/config.yaml');
     command.setCliArgs(args);
+
     await command.run();
     await db.disconnect();
   }
+}
+
+@HelpText('print detailed help for given command')
+abstract class Help extends Command {
+
+  @CliParameter
+  @HelpText('test')
+  late String command;
+
+  Future<void> run() async {
+    String classCode = cli.commandToClassCode(command);
+    if (cli.allCommands.containsKey(classCode)) {
+      var commandObj = cli.allCommands[classCode]!;
+      var parser = cli.getCommandArgParser(commandObj);
+      var annotations = cli.availableCommands.allSubtypes[classCode]!.annotations;
+      String help = annotations.containsKey('HelpText') ? annotations['HelpText'] : '';
+      print(command + ' [options] ' + commandObj.params.map((e) => '[$e]').join(' ') + '\t' + help);
+      print('detailed options:');
+      print(parser.usage);
+    } else {
+      print('unknown command $command');
+    }
+  }
+
 }
