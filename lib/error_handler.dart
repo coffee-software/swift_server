73ab9ba360de58350cd5b5a594fb0a7a53296b3f
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2025-07-21 15:03:06 +0200

    manual error reporting

--- lib/error_handler.dart
+++ lib/error_handler.dart
@@ -11,7 +11,19 @@ abstract class ErrorHandler {
   @Inject
   ServerConfig get config;
 
-  Future handleError(Db db, int appId, String handler, error, stacktrace, {HttpRequest? request, String? requestBody}) async {
+  Future logError(Db db, String message, Map? debug) async {
+    try {
+      throw Exception(message);
+    } on Exception catch (exception, stackTrace) {
+      List<String> lines = stackTrace
+          .toString()
+          .split('\n');
+      String? location = lines.length > 1 ? lines[1] : lines.first;
+      await handleError(db, config.getRequired<int>('service_id'), 'manual', exception, stackTrace, location: location, requestBody: debug != null ? jsonEncode(debug): null);
+    }
+  }
+
+  Future handleError(Db db, int appId, String handler, error, stacktrace, {HttpRequest? request, String? requestBody, String? location}) async {
 
     if (config.getRequired<bool>('debug')) {
       print('###############################################################');
@@ -35,6 +47,11 @@ abstract class ErrorHandler {
     if (requestBody != null) {
       debugRequest += requestBody;
     }
+    String saveLocation = location != null ? location : stacktrace
+        .toString()
+        .split('\n')
+        .first;
+
     await db.query(
         'INSERT INTO run_errors SET '
             '`app_id` = ?, '
@@ -56,10 +73,7 @@ abstract class ErrorHandler {
         [
           appId,
           handler,
-          stacktrace
-              .toString()
-              .split('\n')
-              .first,
+          saveLocation,
           error.toString(),
           stacktrace.toString(),
           debugRequest
